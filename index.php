b15f64a4caa0404cac7847cbe3db312acd7fed8a
Vladimir <vladimir.solomka@gmail.com>
2020-11-28 16:48:48 +0200

    Lesson 2

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+$a = "11";
+echo '<br/>$a: ';
+var_export($a);
+
+$b = 11;
+echo '<br/>$b: ';
+var_export($b);
+
+echo '<br/>$a == $b: ';
+var_export($a == $b);
+
+echo '<br/>$a === $b: ';
+var_export($a === $b);
